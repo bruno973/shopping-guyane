f36ea8d8f0b15b398b065b787d6d3ba77b9a5fa4
bruno973 <sbad973@hotmail.com>
2019-12-06 23:00:38 -0300

    logo fonctionnel

--- source/pie-links.js
+++ source/pie-links.js
@@ -1,6 +1,6 @@
 var data = {
 	'general' : {
-		'logoSrc' : '/.Shopping.png',
+		'logoSrc' : 'Shopping.png',
 		'pageBackgroundColor' : '#EDEDED',
 		'linkBackgroundColor' : '#eec643',
 		'linkTextColor' : '#0A0A0A',
